519cb019407dbe195d15e71c7069f6d36babe082
qua <qualab@gmail.com>
2016-04-05 02:30:45 +0300

    Local scope created to auto register the scope where test checks occurs.

--- sources/test/scope/scope.cpp
+++ sources/test/scope/scope.cpp
@@ -1,9 +1,29 @@
 #include "scope_data"
+#include <stack>
 
 namespace cxc
 {
     namespace test
     {
+        namespace
+        {
+            thread_local std::stack<scope> local_scope;
+
+            class local_scope_entry
+            {
+            public:
+                local_scope_entry(const scope& entry)
+                {
+                    local_scope.push(entry);
+                }
+
+                ~local_scope_entry()
+                {
+                    local_scope.pop();
+                }
+            };
+        }
+
         scope::scope(const std::string& name)
             : m_data(std::make_shared<scope::data>(*this, global(), name))
         {
@@ -29,8 +49,16 @@ namespace cxc
             return global_scope;
         }
 
+        scope& scope::local()
+        {
+            if (local_scope.empty())
+                local_scope.push(global());
+            return local_scope.top();
+        }
+
         void scope::run()
         {
+            local_scope_entry entry(*this);
             return m_data->run();
         }
 
